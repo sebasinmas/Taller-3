a58cea570b88d01d9aaca9046753d79672778523
SebaSinMas <sebastianbustos90@gmail.com>
2024-10-09 15:33:04 -0300

    Agrega los botones y el consumo de API
    Aún no es visualmente bonito

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,75 @@
+<!--eslint-disable no-unused-vars -->
 <script setup>
+import { ref } from "vue";
+import axios from 'axios';
+const dataCountries = ref();
+const dataAnimeRec = ref();
+const dataAnime = ref();
+const dataManga = ref();
+
+
+const getApiCountries = async () => {
+  try {
+    dataCountries.value = await axios.get("https://countriesnow.space/api/v0.1/countries/population");
+    console.log(dataCountries.value.data);
+
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+const getApiAnimeRec = async () => {
+  try {
+    dataAnimeRec.value = await axios.get("https://api.jikan.moe/v4/recommendations/anime");
+    console.log(dataAnimeRec.value.data);
+
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+const getApiAnime = async () => {
+  try {
+    dataAnime.value = await axios.get("https://api.jikan.moe/v4/random/anime");
+    console.log(dataAnime.value.data);
+
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+const getApiManga = async () => {
+  try {
+    dataManga.value = await axios.get("https://api.jikan.moe/v4/random/manga");
+    console.log(dataManga.value.data);
+
+  } catch (e) {
+    console.log(e);
+  }
+}
+
 </script>
 
 <template>
   <header>
+    <div class="paises">
+      <button @click="getApiCountries">Conseguir países</button>
+      <p>{{ dataCountries }}</p>
+
+    </div>
+    <div class="anime-recommendations">
+      <button @click="getApiAnimeRec">Conseguir animes recomendados</button>
+      <p>{{ dataAnimeRec }}</p>
+    </div>
+    <div class="anime">
+      <button @click="getApiAnime">Conseguir animes</button>
+      <p>{{ dataAnime }}</p>
+    </div>
+    <div class="manga">
+      <button @click="getApiManga">Conseguir mangas</button>
+      <p>{{ dataManga }}</p>
+    </div>
+
   </header>
 </template>
 
